1d85149a5b3bfdbbe5e0ec3ba0d1ad9dfc92ae94
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-14 15:37:02 -0500

    Update Nuevo mundo.py

--- pages/Nuevo mundo.py
+++ pages/Nuevo mundo.py
@@ -49,7 +49,7 @@ def text_to_speech(text, tld):
 result, output_text = text_to_speech(text, tld)
 audio_file = open(f"temp/{result}.mp3", "rb")
 audio_bytes = audio_file.read()
-st.markdown(f"## Escucha la historia:")
+st.markdown(f"## Escucha su historia:")
 st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
    # if display_output_text:
