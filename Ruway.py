dad9174bf2765ad405fd1af12456d4881a4e9d94
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-14 12:34:25 -0500

    Update Ruway.py

--- Ruway.py
+++ Ruway.py
@@ -1,2 +1,10 @@
 import streamlit as st
+from PIL import Image
+
 st.title("Proyecto de cosplay Ruway")
+
+st.header("Este proyecto desarrollado para")
+st.write("")
+image = Image.open("imagen 1.jpg")
+
+st.image(image, caption="Interfaces multimodales")
