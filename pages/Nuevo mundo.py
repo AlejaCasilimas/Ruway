b9b61b1dd5b946bb39542bf15afac07f71eea094
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-14 15:30:13 -0500

    Update Nuevo mundo.py

--- pages/Nuevo mundo.py
+++ pages/Nuevo mundo.py
@@ -44,27 +44,27 @@ def text_to_speech(text, tld):
 
 #display_output_text = st.checkbox("Verifica el texto")
 
-if st.button("convertir"):
-    result, output_text = text_to_speech(text, tld)
-    audio_file = open(f"temp/{result}.mp3", "rb")
-    audio_bytes = audio_file.read()
-    st.markdown(f"## Tú audio:")
-    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+#if st.button("convertir"):
+ #   result, output_text = text_to_speech(text, tld)
+ #   audio_file = open(f"temp/{result}.mp3", "rb")
+ #   audio_bytes = audio_file.read()
+ #   st.markdown(f"## Tú audio:")
+ #   st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
     #if display_output_text:
-    st.markdown(f"## Texto en audio:")
-    st.write(f" {output_text}")
+  #  st.markdown(f"## Texto en audio:")
+  #  st.write(f" {output_text}")
 
 
-def remove_files(n):
-    mp3_files = glob.glob("temp/*mp3")
-    if len(mp3_files) != 0:
-        now = time.time()
-        n_days = n * 86400
-        for f in mp3_files:
-            if os.stat(f).st_mtime < now - n_days:
-                os.remove(f)
-                print("Deleted ", f)
+#def remove_files(n):
+ #   mp3_files = glob.glob("temp/*mp3")
+ #   if len(mp3_files) != 0:
+  #      now = time.time()
+   #     n_days = n * 86400
+    #    for f in mp3_files:
+     #       if os.stat(f).st_mtime < now - n_days:
+      #          os.remove(f)
+       #         print("Deleted ", f)
 
 
 remove_files(7)
