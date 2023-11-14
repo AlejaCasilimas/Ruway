e8cc479c9610b0de01517ef44d5b6b5d94d1b8e0
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-14 12:43:09 -0500

    Update Ruway.py

--- Ruway.py
+++ Ruway.py
@@ -3,7 +3,9 @@ from PIL import Image
 
 st.title("Proyecto de cosplay Ruway")
 
-st.header("Este proyecto desarrollado para")
+st.header("Proyecto de cosplay original diseñado y desarrollado por la Universidad EAFIT")
 st.write("")
 image = Image.open("bocetación.png")
+
+st.image(image, caption="Ruway")
 
