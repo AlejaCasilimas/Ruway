c453c2779c7e1517f49a52b3f158e812ec2659a5
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-21 10:40:22 -0500

    Update Ruway.py

--- Ruway.py
+++ Ruway.py
@@ -4,8 +4,15 @@ from PIL import Image
 st.title("Proyecto de cosplay Ruway")
 
 st.header("Proyecto de cosplay original diseñado y desarrollado por la Universidad EAFIT")
-st.write("")
-image = Image.open("bocetación.png")
 
+image = Image.open("bocetación.png")
 st.image(image, caption="Ruway")
+
+st.write("Para saber más acerca del proyeto, ingresa en el siguiente link")
+
+link_text = "[Más información](https://sites.google.com/view/proyectoqijtikal/inicioL)"
+
+# Mostrar el hipervínculo utilizando st.markdown
+st.markdown(link_text, unsafe_allow_html=True)
+
 
