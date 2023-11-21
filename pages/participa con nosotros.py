f298b7cf3988724568d38996e1b5ccaa9b7c700a
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-21 10:23:33 -0500

    Update participa con nosotros.py

--- pages/participa con nosotros.py
+++ pages/participa con nosotros.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 st.title("Participa con nosotros para ser uno de los ganadores")
 
-st.header("Comsursa por un increible sticker que tenemos para tenemos para ti")
-st.write("Solo tienes que ingresar en el siguiente código Qr o en el link que aparece a cointinuación y contestar unas pequeñas preguntas")
+st.header("Consursa por un increible sticker que tenemos para tenemos para ti")
+st.write("Solo tienes que ingresar en el siguiente código Qr o en el link que aparece a continuación y contestar unas pequeñas preguntas")
 image = Image.open("Código QR para Concurso.png")
 
 st.image(image, caption="https://forms.office.com/r/tJ57RUQPAL")
