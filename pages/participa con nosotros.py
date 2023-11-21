3e70c7aa32ec20d2b70748d7425cd8770e471206
AlejaCasilimas <143103499+AlejaCasilimas@users.noreply.github.com>
2023-11-21 10:34:17 -0500

    Update participa con nosotros.py

--- pages/participa con nosotros.py
+++ pages/participa con nosotros.py
@@ -10,7 +10,7 @@ image = Image.open("Código QR para Concurso.png")
 st.image(image, caption="https://forms.office.com/r/tJ57RUQPAL")
 
 # Texto con hipervínculo
-link_text = "[Enlace a Google](https://forms.office.com/r/tJ57RUQPAL)"
+link_text = "[Enlace para concursar](https://forms.office.com/r/tJ57RUQPAL)"
 
 # Mostrar el hipervínculo utilizando st.markdown
 st.markdown(link_text, unsafe_allow_html=True)
